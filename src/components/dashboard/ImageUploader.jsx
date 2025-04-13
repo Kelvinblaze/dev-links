fbd3c7c8d509bddb5cc9733e1c1ec92b826f402f
Ifeanyi Kelvin Ossai <ifeanyiko4u@gmail.com>
2025-04-13 14:55:48 +0100

    implemented custom image uploader  & profile page layout

---
+++ src/components/dashboard/ImageUploader.jsx
@@ -0,0 +1,76 @@
+import { useRef, useState } from "react";
+import { PiImage } from "react-icons/pi";
+
+const ImageUploader = ({ label, onChange }) => {
+  const fileInputRef = useRef(null);
+  const [preview, setPreview] = useState(null);
+
+  const handleClick = () => {
+    if (preview === null) {
+      fileInputRef.current?.click();
+    } else {
+      setPreview(null);
+      if (fileInputRef.current) {
+        fileInputRef.current.value = null;
+      }
+    }
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setPreview(reader.result);
+        if (onChange) onChange(file);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div className="flex flex-col md:flex-row gap-5 items-start md:items-center">
+      <div
+        onClick={handleClick}
+        className="cursor-pointer flex-none size-[193px] bg-light-purple rounded-lg grid place-content-center "
+      >
+        {preview ? (
+          <div className="size-[193px] relative group">
+            <img
+              src={preview}
+              alt="Preview"
+              className="object-cover size-full rounded-lg"
+            />
+            <div className="p-5 absolute top-0 left-0 size-full bg-black/50 rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 grid place-content-center">
+              <div className="space-y-3 text-center">
+                <PiImage size={50} className="text-white w-max mx-auto" />
+                <button className="text-white font-semibold">
+                  Remove Image
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="space-y-3 text-center p-5 ">
+            <PiImage size={50} className="text-purple w-max mx-auto" />
+            <button className="text-purple font-semibold">
+              + Upload Image
+            </button>
+          </div>
+        )}
+      </div>
+
+      {label && <p className="text-grey">{label}</p>}
+
+      <input
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        className="hidden"
+      />
+    </div>
+  );
+};
+
+export default ImageUploader;
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,61 @@
 import React from "react";
 
+// Components
+import Header from "../components/layout/Header";
+import Input from "../components/ui/Input";
+import ImageUploader from "../components/dashboard/ImageUploader";
+
 const ProfilePage = () => {
-  return <div></div>;
+  const handleImageUpload = (file) => {
+    console.log(file);
+  };
+
+  return (
+    <div className="space-y-10">
+      {/* Header */}
+      <Header
+        title="Profile Details"
+        subtitle="Add your details to create a personal touch to your profile."
+      />
+
+      {/* Profile Image Upload */}
+      <section className="bg-light-grey w-full rounded-lg p-5 grid grid-cols-12 gap-5 items-center">
+        <div className="md:col-span-4 col-span-12">
+          <p className="text-grey">Profile Picture</p>
+        </div>
+        <div className="md:col-span-8 col-span-12">
+          <ImageUploader
+            label="Image must be below 1024x1024px. Use PNG or JPG format."
+            onChange={handleImageUpload}
+          />
+        </div>
+      </section>
+
+      {/* Personal Info  */}
+      <section className="bg-light-grey w-full rounded-lg p-5 grid grid-cols-12 gap-5 items-center">
+        <div className="md:col-span-4 col-span-12">
+          <p className="text-grey">First name*</p>
+        </div>
+        <div className="md:col-span-8 col-span-12">
+          <Input placeholder="Ben" />
+        </div>
+
+        <div className="md:col-span-4 col-span-12">
+          <p className="text-grey">Last name*</p>
+        </div>
+        <div className="md:col-span-8 col-span-12">
+          <Input placeholder="Wright" />
+        </div>
+
+        <div className="md:col-span-4 col-span-12">
+          <p className="text-grey">Email</p>
+        </div>
+        <div className="md:col-span-8 col-span-12">
+          <Input placeholder="ben@example.com" type="email" />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default ProfilePage;
