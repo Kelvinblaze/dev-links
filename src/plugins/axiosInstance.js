6fec8f8fbf6b2001adf0bf7e3e416d2bc0ee6a20
Ifeanyi Kelvin Ossai <ifeanyiko4u@gmail.com>
2025-04-23 20:12:10 +0100

    fix: update axios baseURL to use VITE_BASE_URL for consistency

--- src/plugins/axiosInstance.js
+++ src/plugins/axiosInstance.js
@@ -4,7 +4,7 @@ import { persistor } from "../store";
 import { jwtDecode } from "jwt-decode";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 10000,
 });
 
